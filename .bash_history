b61b5a446ef37176f77903d24dfd91a6f91e614d
VadimZavgorodny <va.zavgorodny@gmail.com>
2018-05-22 21:46:36 +0300

    Added telegram bot application

---
+++ .bash_history
@@ -0,0 +1,9 @@
+composer --version
+composer self-update
+composer install
+ls -lf
+composer install
+lf -ls
+lf -ls
+ls -lf
+composer install
---
+++ config/config.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+return [
+    'telegramBot' => [
+        'botApiKey' => '', //Ключ от папы бота
+        'botUserName' => '' //Имя бота
+    ],
+    'db' => [
+        'host' => 'localhost',
+        'user' => '', //Имя пользователя БД
+        'password' => '', //Пароль от БД
+        'database' => '', //Имя БД
+    ]
+];
